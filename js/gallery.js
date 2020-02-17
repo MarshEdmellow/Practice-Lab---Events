a1bf9f635af62af6a2c47acc9fdca247b2e3ef96
Edmar Bucsit <edmar.bucsitjr.iics@ust.edu.ph>
2020-02-17 21:19:19 +0800

    Second Commit - Gallery

--- js/gallery.js
+++ js/gallery.js
@@ -8,16 +8,17 @@ function upDate(previewPic){
        2) Change the text  of the div with the id = "image" 
        to the alt text of the preview image 
        */
-        var src = previewPic.getAttribute( "src" );
-        var alt = previewPic.getAttribute( "alt" );
-        document.getElementById('image').style.backgroundImage = "url('" + src + "')";
-        document.getElementById('image').innerHTML = alt;
-            
+        let image = "url(" + previewPic.src + ")";
+        let target = document.getElementById("image");
+        let caption = previewPic.alt;
 
-        
+        target.style.backgroundImage = image;
+        target.innerHTML = caption;
+
+
        }
    
-       function unDo(image){
+       function unDo(){
         /* In this function you should 
        1) Update the url for the background image of the div with the id = "image" 
        back to the orginal-image.  You can use the css code to see what that original URL was
@@ -25,10 +26,12 @@ function upDate(previewPic){
        2) Change the text  of the div with the id = "image" 
        back to the original text.  You can use the html code to see what that original text was
        */
-        var image = document.getElementById('image');
-        image.style.backgroundImage = "url('')";
-        document.getElementById('image').innerHTML = "Hover over an image below to display here";
-                
-        }
-           
-       
+        let image = "url()";
+        let target = document.getElementById("image");
+        let defaultText = "Hover over an image below to display here.";
+
+        target.style.backgroundImage = image;
+        target.innerHTML = defaultText;    
+
+
+       }
